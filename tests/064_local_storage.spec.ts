e2c650ed8efac5a2d0982cb69b7d684837a4479b
gauravchauhan200 <gauravchauhan200@gmail.com>
2026-09-23 22:49:21 +0530

    session

--- tests/064_local_storage.spec.ts
+++ tests/064_local_storage.spec.ts
@@ -2,7 +2,6 @@ import { chromium } from "@playwright/test";
 
 async function saveAdminStorage()
 {
-
     let browser = await chromium.launch({headless:false});
     let context = await browser.newContext();
     let page = await context.newPage();
@@ -12,7 +11,7 @@ async function saveAdminStorage()
     await page.getByRole('textbox', { name: 'Username' }).fill('admin');
     await page.getByLabel('Password').fill('admin123');
    
-   // await page.getByLabel('🍪 Cookie').check();
+    // await page.getByLabel('🍪 Cookie').check();
     await page.getByLabel('💾 Local').check();
     await page.getByRole('button',{name:'Login'}).click();
 
@@ -24,7 +23,30 @@ async function saveAdminStorage()
     await browser.close();
 }
 
+    async function saveUserStorage()
+    {
+        let browser = await chromium.launch({headless:false})
+        let context = await browser.newContext();
+        let page = await context.newPage()
 
-    //Calling the functions
+        // Login
+        await page.goto('https://sdetqa.vercel.app/login_app');
+        await page.getByRole('textbox',{name:'Username'}).fill('testuser1');
+        await page.getByRole('textbox',{name: 'Password'}).fill('testuser123');
+        
+        // await page.getByLabel('🍪 Cookie').check();
+        await page.getByLabel('💾 Local').check();
+        await page.getByRole('button', { name: 'Login' }).click();
 
-    saveAdminStorage();
+        await page.waitForSelector('text=Dashboard Welcome', { state: 'visible' });
+
+        // able to capture both cookies storage and local storage
+        await context.storageState({ path: './storage_data/user_data.json' });
+        await browser.close();
+     
+    }
+
+        //Calling the functions
+        
+        saveAdminStorage();
+        saveUserStorage();
