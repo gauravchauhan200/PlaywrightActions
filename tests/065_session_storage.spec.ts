ef4af629932a697c8ca48e04b4d531301f58ade2
gauravchauhan200 <gauravchauhan200@gmail.com>
2026-09-24 17:00:57 +0530

    session_data

--- tests/065_session_storage.spec.ts
+++ tests/065_session_storage.spec.ts
@@ -3,6 +3,38 @@ import fs from 'fs'
 
 async function saveSessionStorage()
 {
+    const browser = await chromium.launch({ headless: false});
 
+    // Create browser context
     
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    // Open application
+    await page.goto('https://sdetqa.vercel.app/login_app.html');
+
+    // Login
+    await page.getByRole('textbox', { name: 'Username' }).fill('admin');
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+
+    // Select session storage option
+    await page.getByLabel('⏳ Session').check();
+
+    //Click login
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.waitForSelector('text=Dashboard Welcome', { state: 'visible' });
+
+    // capture session storage data
+    const sessionStorageData = await page.evaluate(()=>{
+       return sessionStorage; 
+    })
+
+    // Save session storage to file
+    fs.writeFileSync("./storage_data/session_data.json",JSON.stringify(sessionStorageData));
+
+    console.log("session data captured....");
+    await browser.close()
+   
 }
+
+    saveSessionStorage();
